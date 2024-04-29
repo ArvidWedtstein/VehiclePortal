f4054acf84c2d72858877a57509ee197ce13277b
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2024-04-29 21:03:58 +0200

    dev

--- theme.ts
+++ theme.ts
@@ -3,7 +3,7 @@ import { Roboto } from "next/font/google";
 import { createTheme } from "@mui/material/styles";
 
 const roboto = Roboto({
-  weight: ["300", "400", "500", "700"],
+  weight: ["100", "300", "400", "500", "700"],
   subsets: ["latin"],
   display: "swap",
 });
